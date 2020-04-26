a6400e584cd8441ed9e556f33575a3f8e93a5f58
Dillonmemo <arta1069@gmail.com>
2020-04-26 22:19:12 +0900

    Test API

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
+import Axios from "axios";
 
 export interface NewsProps {}
 
 const News: React.FC<NewsProps> = ({}) => {
+  const [quote, setQuote] = useState<string>("");
+
+  useEffect(() => {
+    setQuote("test");
+    Axios.get("http://horizonshq.herokuapp.com/api/inspirationalquotes")
+      .then((response) => {
+        console.log(response);
+        setQuote(response.data.message);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>Title | News</title>
       </Helmet>
-      <div>News__</div>
+      <div>
+        News__
+        <p>{quote}</p>
+      </div>
     </>
   );
 };
